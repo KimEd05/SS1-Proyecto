49e6709c45fb9ce2a9300333b32009624f4beb05
Kimberly Elias <kim.eliasdiaz@gmail.com>
2020-11-07 17:17:51 -0600

    Candidates loaded

---
+++ js/index.js
@@ -0,0 +1,116 @@
+navigator.mediaDevices.getUserMedia(
+    {
+        video: true
+    }
+
+).then((strem) =>{
+    let webcam = document.getElementById('webcam')
+    webcam.srcObject = strem
+    
+}).catch((err) =>{
+    console.log(err)
+})
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+
+function getDataUrl(img) {
+    // Create canvas
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    // Set width and height
+    canvas.width = img.width;
+    canvas.height = img.height;
+    // Draw the image
+    ctx.drawImage(img, 0, 0);
+    return canvas.toDataURL('image/jpeg');
+}
+
+async function iniciarSesionCam() {
+    /*var video = document.getElementById('webcam');
+    
+    const canvas = document.createElement("canvas");
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const image = new Image()
+    image.src = canvas.toDataURL();
+    console.log(image.src);
+    var usuario = document.getElementById("usuario").value;
+    var raw = JSON.stringify({"user":usuario, "password":"", "foto":image.src});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    fetch("https://d2mkcjc0c59xy8.cloudfront.net/login", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        var res = JSON.parse(result)
+        if(res.estado === "ok") {
+            window.location = "principal.html";
+        } else {
+            alert("Credenciales incorrectas");
+        }
+    })
+    .catch(error => {
+        alert("Credenciales incorrectas");
+        console.log('error', error)
+    });*/
+    window.location = "principal.html";
+
+}
+
+function registrarse() {
+    window.location = "signin.html";
+}
+
+var imagensrc = "";
+function capturarFoto() {
+    var video = document.getElementById('webcam');
+    
+    const canvas = document.createElement("canvas");
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imagensrc = canvas.toDataURL();
+    var captura = document.getElementById("captura");
+    captura.src = imagensrc;
+}
+
+function register() {
+    var usuario = document.getElementById("user-sign").value;
+    var pass = document.getElementById("pass-sign").value;
+    if(pass !== document.getElementById("rep-sign").value) {
+        alert("Las contraseñas no coinciden.");
+    } else {
+        var raw = JSON.stringify({"user":usuario, "password":pass, "foto":imagensrc});
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        fetch("https://d2mkcjc0c59xy8.cloudfront.net/adduser", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            var res = JSON.parse(result)
+            if(res.estado === "ok") {
+                imagensrc = "";
+                alert("Registro finalizado!");
+                window.location = "index.html";
+            } else {
+                alert("Credenciales incorrectas");
+            }
+        })
+        .catch(error => {
+            alert("Credenciales incorrectas");
+            console.log('error', error)
+        });
+    }
+     
+}
---
+++ js/students.js
@@ -0,0 +1,250 @@
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+   
+}
+
+function funcion_inicial()
+{
+    var contenedor = document.getElementById("container-lista-estudiantes");
+    for(var i = 0; i < 7; i++) {
+        var fila = document.createElement("div");
+        fila.className="row";
+
+        for(var j = 0; j < 3; j++) {
+            if(3*i+j < 21) {
+                var col = document.createElement("div");
+                col.className = "col-md-4 col-sm-4";
+
+                var team = document.createElement("div");
+                team.className = "team-thumb";
+
+                var img = document.createElement("img");
+                img.id = "_" + (3*i+j + 1);
+                img.className = "img-responsive";
+                img.src = "https://bucket-proyecto-ss1.s3.us-east-2.amazonaws.com/Papeleta/" + (3*i+j + 1) + ".jpg";
+                
+                team.appendChild(img);
+                col.appendChild(team);
+                fila.appendChild(col);
+            }
+        }
+
+        contenedor.appendChild(fila);
+        contenedor.innerHTML += "<br>"
+    }
+
+    for(var i = 1; i < 22; i++) {
+        document.getElementById("_" + i).addEventListener("click", function() {
+            console.log(this.id.replace("_",""));
+        });
+    }
+    /*var client = new HttpClient();
+    client.get('https://d2mkcjc0c59xy8.cloudfront.net/getstudents', function(response) {
+        var contenedor = document.getElementById("container-lista-estudiantes");
+        var listado = JSON.parse(response).listado;
+        console.log(listado);
+        for(var i = 0; i <= (Math.round(listado.length / 4)+ 1); i++) {
+            var fila = document.createElement("div");
+            fila.className="row";
+
+            for(var j = 0; j < 4; j++) {
+                if(4*i+j < listado.length) {
+                    var col = document.createElement("div");
+                    col.className = "col-md-3 col-sm-3";
+
+                    var team = document.createElement("div");
+                    team.className = "team-thumb";
+
+                    var img = document.createElement("img");
+                    img.className = "img-responsive";
+                    img.src = "https://pro1-images-grupo8.s3.us-east-2.amazonaws.com/" + listado[4*i+j].Foto;
+                                
+                    var info = document.createElement("div");
+                    info.className = "team-info team-thumb-up";
+                    info.innerHTML = "<h3>" + listado[4*i+j].Nombre + "</h3>";
+
+                    team.appendChild(img);
+                    team.appendChild(info);
+                    col.appendChild(team);
+                    fila.appendChild(col);
+                }
+            }
+            contenedor.appendChild(fila);
+            contenedor.innerHTML += "<br>"
+        }
+    });*/
+
+    /*client.get('https://vv3p26ogll.execute-api.us-east-2.amazonaws.com/pro1-g8/serverless/get-grupal', function(response) {
+        var contenedor = document.getElementById("container-lista-grupos");
+        var listado = JSON.parse(response);
+        console.log(listado);
+        for(var i = 0; i <= (Math.round(listado.length / 4)+ 1); i++) {
+            var fila = document.createElement("div");
+            fila.className="row";
+
+            for(var j = 0; j < 4; j++) {
+                if(4*i+j < listado.length) {
+                    var col = document.createElement("div");
+                    col.className = "col-md-3 col-sm-3";
+
+                    var team = document.createElement("div");
+                    team.className = "team-thumb";
+
+                    var img = document.createElement("img");
+                    img.className = "img-responsive";
+                    img.src = "https://pro1-images-grupo8.s3.us-east-2.amazonaws.com/" + listado[4*i+j].Foto.S;
+                                
+                    var info = document.createElement("div");
+                    info.className = "team-info team-thumb-up";
+                    info.innerHTML = "<h3>" + listado[4*i+j].Id_foto.S + "</h3>";
+
+                    team.appendChild(img);
+                    team.appendChild(info);
+                    col.appendChild(team);
+                    fila.appendChild(col);
+                }
+            }
+            contenedor.appendChild(fila);
+            contenedor.innerHTML += "<br>"
+        }
+    });*/
+
+    /*client.get('https://vv3p26ogll.execute-api.us-east-2.amazonaws.com/pro1-g8/serverless/get-asistencias', function(response) {
+        var contenedor = document.getElementById("container-lista-asistencias");
+        var listado = JSON.parse(response);
+        console.log(listado);
+        
+        for(var i = 0; i < listado.length ; i++) {
+            var fila = document.createElement("div");
+            fila.innerHTML =    "<h3><center>" + listado[i]['Grupo'] + "</center></h3>" +
+                                "<h2><a href=\"https://pro1-images-grupo8.s3.us-east-2.amazonaws.com/" + listado[i]['Foto_Grupo'] + "\" target=\"_blank\">Ver Imagen</a></h2>";
+            
+            var table = document.createElement("table");
+            table.className = "tabcontainer";
+
+            var thead = document.createElement("thead");
+            thead.innerHTML +=  "<tr>"+
+                                        "<th><h1><center>Estudiante</center></h1></th>"+
+                                        "<th><h1><center>Imagen Estudiante</center></h1></th>"+
+                                        "<th><h1><center>Asistió</center></h1></th>"+
+                                "</tr>";
+
+            var tbody = document.createElement("tbody");
+                for(var j = 0; j < listado[i]['Asistencia'].length ; j++){
+                    tbody.innerHTML +=  "<tr>"+
+                                            "<td><center>" + listado[i]['Asistencia'][j]['Estudiante']['S'] + "</center></td>"+
+                                            "<td><center><a href=\"https://pro1-images-grupo8.s3.us-east-2.amazonaws.com/" + listado[i]['Asistencia'][j]['Foto_Estudiante']['S'] + "\" target=\"_blank\">Ver</a></center></td>"+
+                                            "<td><center>" + listado[i]['Asistencia'][j]['asistencia']['S'] + "</center></td>"+
+                                        "</tr>";
+                }
+
+            
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            fila.appendChild(table);
+              
+            contenedor.appendChild(fila);
+            contenedor.innerHTML += "<br><br>"
+        }
+    });*/
+}
+
+function promptFile(contentType, multiple) {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.multiple = multiple;
+    input.accept = contentType;
+    return new Promise(function(resolve) {
+      document.activeElement.onfocus = function() {
+        document.activeElement.onfocus = null;
+        setTimeout(resolve, 100);
+      };
+      input.onchange = function() {
+        var files = Array.from(input.files);
+        if (multiple)
+          return resolve(files);
+        resolve(files[0]);
+      };
+      input.click();
+    });
+}
+
+function promptFilename() {
+    var img = document.getElementById("img-estudiante");
+    promptFile().then(function(file) {
+        img.src = URL.createObjectURL(file);
+    });
+}
+
+function getDataUrl(img) {
+    // Create canvas
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    // Set width and height
+    canvas.width = img.width;
+    canvas.height = img.height;
+    // Draw the image
+    ctx.drawImage(img, 0, 0);
+    return canvas.toDataURL('image/jpeg');
+}
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+async function registrarEstudiante() {
+    var img = document.getElementById("img-estudiante");
+    var id = document.getElementById("id-estudiante").value;
+    var raw = JSON.stringify({"id":id,"foto":getDataUrl(img)});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+    fetch("https://d2mkcjc0c59xy8.cloudfront.net/addstudent", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .then(function () {
+        
+    location.reload();
+    })
+    .catch(error => console.log('error', error));
+}
+
+function promptFilenameGrupo() {
+    var img = document.getElementById("img-grupo");
+    promptFile().then(function(file) {
+        img.src = URL.createObjectURL(file);
+    });
+}
+
+async function registrarGrupo() {
+    var img = document.getElementById("img-grupo");
+    var id = document.getElementById("id-grupo").value;
+    var raw = JSON.stringify({"name":id,"sourceBase64":getDataUrl(img)});
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+    fetch("https://vv3p26ogll.execute-api.us-east-2.amazonaws.com/pro1-g8/serverless/upload-grupal", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .then(function () {
+        
+    location.reload();
+    })
+    .catch(error => console.log('error', error));
+}
