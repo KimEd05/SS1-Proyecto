bc559ef738eddf3f2de8c0eaeec560087410015e
Kimberly Elias <kim.eliasdiaz@gmail.com>
2020-11-07 19:30:41 -0600

    Voting flow

--- js/students.js
+++ js/students.js
@@ -12,6 +12,7 @@ var HttpClient = function() {
    
 }
 
+
 function funcion_inicial()
 {
     var contenedor = document.getElementById("container-lista-estudiantes");
@@ -47,83 +48,12 @@ function funcion_inicial()
             console.log(this.id.replace("_",""));
         });
     }
-    /*var client = new HttpClient();
-    client.get('https://d2mkcjc0c59xy8.cloudfront.net/getstudents', function(response) {
-        var contenedor = document.getElementById("container-lista-estudiantes");
-        var listado = JSON.parse(response).listado;
-        console.log(listado);
-        for(var i = 0; i <= (Math.round(listado.length / 4)+ 1); i++) {
-            var fila = document.createElement("div");
-            fila.className="row";
-
-            for(var j = 0; j < 4; j++) {
-                if(4*i+j < listado.length) {
-                    var col = document.createElement("div");
-                    col.className = "col-md-3 col-sm-3";
-
-                    var team = document.createElement("div");
-                    team.className = "team-thumb";
-
-                    var img = document.createElement("img");
-                    img.className = "img-responsive";
-                    img.src = "https://pro1-images-grupo8.s3.us-east-2.amazonaws.com/" + listado[4*i+j].Foto;
-                                
-                    var info = document.createElement("div");
-                    info.className = "team-info team-thumb-up";
-                    info.innerHTML = "<h3>" + listado[4*i+j].Nombre + "</h3>";
-
-                    team.appendChild(img);
-                    team.appendChild(info);
-                    col.appendChild(team);
-                    fila.appendChild(col);
-                }
-            }
-            contenedor.appendChild(fila);
-            contenedor.innerHTML += "<br>"
-        }
-    });*/
-
-    /*client.get('https://vv3p26ogll.execute-api.us-east-2.amazonaws.com/pro1-g8/serverless/get-grupal', function(response) {
-        var contenedor = document.getElementById("container-lista-grupos");
-        var listado = JSON.parse(response);
-        console.log(listado);
-        for(var i = 0; i <= (Math.round(listado.length / 4)+ 1); i++) {
-            var fila = document.createElement("div");
-            fila.className="row";
-
-            for(var j = 0; j < 4; j++) {
-                if(4*i+j < listado.length) {
-                    var col = document.createElement("div");
-                    col.className = "col-md-3 col-sm-3";
-
-                    var team = document.createElement("div");
-                    team.className = "team-thumb";
-
-                    var img = document.createElement("img");
-                    img.className = "img-responsive";
-                    img.src = "https://pro1-images-grupo8.s3.us-east-2.amazonaws.com/" + listado[4*i+j].Foto.S;
-                                
-                    var info = document.createElement("div");
-                    info.className = "team-info team-thumb-up";
-                    info.innerHTML = "<h3>" + listado[4*i+j].Id_foto.S + "</h3>";
-
-                    team.appendChild(img);
-                    team.appendChild(info);
-                    col.appendChild(team);
-                    fila.appendChild(col);
-                }
-            }
-            contenedor.appendChild(fila);
-            contenedor.innerHTML += "<br>"
-        }
-    });*/
 
     var client = new HttpClient();
     client.get('https://yfsgst38xj.execute-api.us-east-2.amazonaws.com/develop/serverless/getVotos', function(response) {
         var contenedor = document.getElementById("container-lista-asistencias");
         var listado = JSON.parse(response);
-        console.log(listado);
-            
+        
         var table = document.createElement("table");
         table.className = "tabcontainer";
 
@@ -156,31 +86,9 @@ function funcion_inicial()
     });
 }
 
-function promptFile(contentType, multiple) {
-    var input = document.createElement("input");
-    input.type = "file";
-    input.multiple = multiple;
-    input.accept = contentType;
-    return new Promise(function(resolve) {
-      document.activeElement.onfocus = function() {
-        document.activeElement.onfocus = null;
-        setTimeout(resolve, 100);
-      };
-      input.onchange = function() {
-        var files = Array.from(input.files);
-        if (multiple)
-          return resolve(files);
-        resolve(files[0]);
-      };
-      input.click();
-    });
-}
-
-function promptFilename() {
-    var img = document.getElementById("img-estudiante");
-    promptFile().then(function(file) {
-        img.src = URL.createObjectURL(file);
-    });
+function vote(id) 
+{
+    console.log("Hola");
 }
 
 function getDataUrl(img) {
@@ -217,13 +125,6 @@ async function registrarEstudiante() {
     location.reload();
     })
     .catch(error => console.log('error', error));
-}
-
-function promptFilenameGrupo() {
-    var img = document.getElementById("img-grupo");
-    promptFile().then(function(file) {
-        img.src = URL.createObjectURL(file);
-    });
 }
 
 async function registrarGrupo() {
